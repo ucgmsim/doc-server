abaf78d3bb77d136d17d1b339d16e187ef6eadec
Jake Faulkner <jakefaulkn@gmail.com>
2025-02-20 16:09:33 +1300

    handle weirdly packaged python repos

--- update_docs.sh
+++ update_docs.sh
@@ -29,8 +29,10 @@ done < /repos.txt
 echo "Generating documentation with pydoctor..."
 rm -rf "$DOCS_DIR/*"
 cd "$REPOS_DIR"
-for repo in "$REPOS_DIR"/*; do
-    [ -d "$repo" ] || continue  # Skip if not a directory
-    repo_name=$(basename "$repo")
-    echo "$repo/$repo_name"
-done | xargs pydoctor --project-name "QuakeCoRE" --html-output="$DOCS_DIR" --docformat=numpy --project-version="$(date +%Y-%m-%dT%H:%M)" --intersphinx=https://docs.python.org/3/objects.inv
+while read -r line; do
+    [ -z "$line" ] && continue  # Skip empty lines
+
+    repo_name=$(echo "$line" | cut -d'@' -f1 | xargs)  # Extract repo name (trim spaces)
+    repo_path=$(echo "$line" | cut -d'@' -f3 | xargs)
+    echo "$REPOS_DIR/$repo_name/$repo_path"
+done < /repos.txt | xargs pydoctor --project-name "QuakeCoRE" --html-output="$DOCS_DIR" --docformat=numpy --project-version="$(date +%Y-%m-%dT%H:%M)" --intersphinx=https://docs.python.org/3/objects.inv
